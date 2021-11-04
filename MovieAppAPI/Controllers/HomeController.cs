f6e80b8a720dcc18b98e7dfd386bfec92a01685d
Chris Paul <cpaul0013@gmail.com>
2021-11-04 08:56:51 -0400

    Committed

---
+++ MovieAppAPI/Controllers/HomeController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MovieAppAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieAppAPI.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        private MovieDAL movieDAL = new MovieDAL();
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult MovieSearchResult(string keyword)
+        {
+            MovieModel result = movieDAL.GetMovie(keyword);
+            return View(result);
+        }
+        public IActionResult MovieNight()
+        {
+            
+            
+            return View();
+        }
+
+        public IActionResult MovieSearch()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult MovieNight(string keyword1, string keyword2, string keyword3)
+        {
+            MovieModel result1 = movieDAL.GetMovie(keyword1);
+            MovieModel result2 = movieDAL.GetMovie(keyword2);
+            MovieModel result3 = movieDAL.GetMovie(keyword3);
+            List<MovieModel> movieModelList = new List<MovieModel>();
+            movieModelList.Add(result1);
+            movieModelList.Add(result2);
+            movieModelList.Add(result3);
+            return View(movieModelList);
+        }
+     
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MovieAppAPI/Models/MovieDAL.cs
@@ -0,0 +1,47 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace MovieAppAPI.Models
+{
+    public class MovieDAL
+    {
+        public MovieModel GetMovie(string keyword)
+        {// urls in C# need https://
+            string url = $"http://www.omdbapi.com/?apikey=93fc2d72&t={keyword}";
+
+
+
+
+            // request is called request
+            HttpWebRequest request = WebRequest.CreateHttp(url);
+
+            //response is called response
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            //this will allow it to read the response but won't be readable for humans
+            StreamReader reader = new StreamReader(response.GetResponseStream());
+
+            //This converts the data read in the stream reader into a c# class
+            string JSON = reader.ReadToEnd();
+
+            // takes the string and turns it into an object
+            MovieModel result = JsonConvert.DeserializeObject<MovieModel>(JSON);
+            return result;
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+}
